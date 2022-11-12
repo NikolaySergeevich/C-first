dc8f43a181a084ab0ec37362c7d2f57b48ff33e9
Nikolay <nik.pavlovskiy.1997@mail.ru>
2022-11-12 21:37:45 +0300

    Написал код приветствие того, кто представился

--- Задания/Задание2_HelloNik/Program.cs
+++ Задания/Задание2_HelloNik/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя");
+string name = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(name);
