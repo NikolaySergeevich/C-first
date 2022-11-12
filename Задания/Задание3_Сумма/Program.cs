b6e46acbb309b1e29a9139f8d64864bfde28c0c0
Nikolay <nik.pavlovskiy.1997@mail.ru>
2022-11-12 21:55:06 +0300

    Сумма рандома

--- Задания/Задание3_Сумма/Program.cs
+++ Задания/Задание3_Сумма/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 14;
-int numberB = 2;
+﻿int numberA = new Random().Next(1,9);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,9);
+Console.WriteLine(numberB);
 int sum = numberA + numberB;
 Console.WriteLine(sum);
